6a6b940c7c2adc0512d7302e4365e13b0896d044
Mausoomi Roy <112415870+Mausoomi12@users.noreply.github.com>
2023-01-05 17:44:20 +0530

    Time: 16 ms (43.91%), Space: 5.9 MB (79.26%) - LeetHub

---
+++ 0012-integer-to-roman/0012-integer-to-roman.cpp
@@ -0,0 +1,60 @@
+class Solution {
+public:
+    string intToRoman(int num) {
+  std::string result = "";
+  while (num >= 1000) {
+    result += "M";
+    num -= 1000;
+  }
+  while (num >= 900) {
+    result += "CM";
+    num -= 900;
+  }
+  while (num >= 500) {
+    result += "D";
+    num -= 500;
+  }
+  while (num >= 400) {
+    result += "CD";
+    num -= 400;
+  }
+  while (num >= 100) {
+    result += "C";
+    num -= 100;
+  }
+  while (num >= 90) {
+    result += "XC";
+    num -= 90;
+  }
+  while (num >= 50) {
+    result += "L";
+    num -= 50;
+  }
+  while (num >= 40) {
+    result += "XL";
+    num -= 40;
+  }
+  while (num >= 10) {
+    result += "X";
+    num -= 10;
+  }
+  while (num >= 9) {
+    result += "IX";
+    num -= 9;
+  }
+  while (num >= 5) {
+    result += "V";
+    num -= 5;
+  }
+  while (num >= 4) {
+    result += "IV";
+    num -= 4;
+  }
+  while (num >= 1) {
+    result += "I";
+    num -= 1;
+  }
+  return result;
+  
+    }
+};

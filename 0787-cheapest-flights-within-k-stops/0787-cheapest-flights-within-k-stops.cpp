9fdb7d11d626629a857c88db7e98e1883c71aa0e
Mausoomi Roy <112415870+Mausoomi12@users.noreply.github.com>
2023-01-26 12:08:56 +0530

    Time: 20 ms (93.39%), Space: 13.2 MB (68.13%) - LeetHub

---
+++ 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int findCheapestPrice(int n, vector<vector<int>>& edges, int st, int en, int k) {
+        vector<int> price(n, 1e9);
+        vector<vector<pair<int, int>>> adj(n);
+        queue<pair<int, pair<int, int>>> q;
+        for (vector<int>& v : edges) 
+            adj[v[0]].push_back({v[1], v[2]});
+        q.push({st, {0, 0}});
+        price[st] = 0;
+        while (!q.empty()) {
+            int cur = q.front().first;
+            int curprice = q.front().second.first;
+            int moves = q.front().second.second;
+            q.pop();
+            if (moves > k)
+                continue;
+            for (pair<int, int>& p : adj[cur]) {
+                if (price[p.first] <= curprice + p.second)
+                    continue;
+                q.push({p.first, {curprice + p.second, moves + 1}});
+                price[p.first] = curprice + p.second;
+            }
+        }
+        return price[en] == 1e9 ? -1 : price[en];
+    }
+};

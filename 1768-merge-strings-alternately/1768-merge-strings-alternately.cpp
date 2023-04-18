fbd4674bf1d00fcd761637e96a8a86f8f287204e
Mausoomi Roy <112415870+Mausoomi12@users.noreply.github.com>
2023-04-18 23:06:37 +0530

    Time: 0 ms (100.00%), Space: 6.2 MB (98.18%) - LeetHub

---
+++ 1768-merge-strings-alternately/1768-merge-strings-alternately.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    string mergeAlternately(string word1, string word2) {
+
+        string ans = "";
+        int p1 = 0, p2 = 0;
+        int n = word1.length(), m = word2.length();
+        int i = 0;
+        while(p1 != n && p2 != m) {
+            if (i%2==1) {
+                ans += word2[p2++];
+            } else {
+                ans += word1[p1++];
+            }
+            i++;
+        }
+        if (p1 != n) {
+            ans += word1.substr(p1, n);
+        }
+        if (p2 != m) {
+            ans += word2.substr(p2, m);
+        }
+        return ans;
+    }
+};

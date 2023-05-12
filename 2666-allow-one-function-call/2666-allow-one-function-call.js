8479f425141ec4753e8473a26f6a44c1811d2d11
Mausoomi Roy <112415870+Mausoomi12@users.noreply.github.com>
2023-05-12 22:32:22 +0530

    Time: 52 ms (88.74%), Space: 42 MB (41.00%) - LeetHub

---
+++ 2666-allow-one-function-call/2666-allow-one-function-call.js
@@ -0,0 +1,34 @@
+/**
+ * @param {Function} fn
+ * @return {Function}
+ */
+var once = function(fn) {
+
+  let hasBeenCalled = false;
+  let result;
+
+  return function(...args) {
+    if (!hasBeenCalled) {
+      result = fn(...args);
+      hasBeenCalled = true;
+      return result;
+    } else {
+      return undefined;
+    }
+  }
+
+};
+
+let fn = (a,b,c) => (a + b + c);
+let onceFn = once(fn);
+
+console.log(onceFn(1,2,3)); // 6
+console.log(onceFn(2,3,6)); // undefined
+
+/**
+ * let fn = (a,b,c) => (a + b + c)
+ * let onceFn = once(fn)
+ *
+ * onceFn(1,2,3); // 6
+ * onceFn(2,3,6); // returns undefined without calling fn
+ */

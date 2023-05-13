3fe6c3b5dff21437fb5777958c1151df2b976a3d
Mausoomi Roy <112415870+Mausoomi12@users.noreply.github.com>
2023-05-13 22:27:05 +0530

    Time: 337 ms (89.63%), Space: 106.7 MB (33.51%) - LeetHub

---
+++ 2623-memoize/2623-memoize.js
@@ -0,0 +1,41 @@
+/**
+ * @param {Function} fn
+ */
+function memoize(fn) {
+    
+   const cache = {};
+  
+   return function(...args) {
+    const key = JSON.stringify(args);
+    
+    if (key in cache) {
+      return cache[key];
+    }
+    
+    const result = fn.apply(this, args);
+    cache[key] = result;
+    
+    return result;
+  }
+  
+}
+
+
+const memoizedSum = memoize(function(a, b) {
+  return a + b;
+});
+
+console.log(memoizedSum(2, 3)); // Output: Computing sum, 5
+console.log(memoizedSum(2, 3)); // Output: 5
+
+
+/** 
+ * let callCount = 0;
+ * const memoizedFn = memoize(function (a, b) {
+ *	 callCount += 1;
+ *   return a + b;
+ * })
+ * memoizedFn(2, 3) // 5
+ * memoizedFn(2, 3) // 5
+ * console.log(callCount) // 1 
+ */

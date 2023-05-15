d6edf14605931ba57113fbc09d4fee3cd86bcb13
Mausoomi Roy <112415870+Mausoomi12@users.noreply.github.com>
2023-05-15 22:40:35 +0530

    Time: 54 ms (83.97%), Space: 42.1 MB (27.35%) - LeetHub

---
+++ 2621-sleep/2621-sleep.js
@@ -0,0 +1,12 @@
+/**
+ * @param {number} millis
+ */
+async function sleep(millis) {
+  return new Promise(delayresolve => setTimeout(delayresolve, millis));
+}
+
+
+/** 
+ * let t = Date.now()
+ * sleep(100).then(() => console.log(Date.now() - t)) // 100
+ */

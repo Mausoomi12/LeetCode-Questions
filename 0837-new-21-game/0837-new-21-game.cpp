fc9422f4b4ed29cd4af8fc203aa1a97c90758304
Mausoomi Roy <112415870+Mausoomi12@users.noreply.github.com>
2023-05-25 22:32:02 +0530

    Time: 0 ms (100.00%), Space: 9.1 MB (78.23%) - LeetHub

---
+++ 0837-new-21-game/0837-new-21-game.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    double new21Game(int n, int k, int maxPts) {
+        if (k == 0 || n >= k + maxPts) {
+            return 1.0;
+        }
+        vector<double> dp(n + 1);
+        dp[0] = 1.0;
+        double sum = 1.0, res = 0.0;
+        for (int i = 1; i <= n; i++) {
+            dp[i] = sum / maxPts;
+            if (i < k){
+                sum += dp[i];
+            } 
+            else{
+                res += dp[i];
+            }
+            if (i >= maxPts) {
+                sum -= dp[i - maxPts];
+            }
+        }
+        return res;
+    }
+};
